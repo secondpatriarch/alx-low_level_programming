94f7983bab7553d34aeef8b8091b515691552200
Isaac Adigwe <isaacadigwe@gmail.com>
2022-12-23 13:40:57 -0800

    working now :)

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * sign - prints the sign of a number
+ * print_sign - prints the sign of a number
  *
  * @n: int value to be compared
  *
@@ -15,12 +15,12 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		return(1);
+		return (1);
 	}
 	if (n == 0)
 	{
 		_putchar('0');
-		return(0);
+		return (0);
 	}
 	else
 	{
