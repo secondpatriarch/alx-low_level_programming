ec20b4e2d29acd4718f1b2eb5ccb9db4840b9db2
Isaac Adigwe <isaacadigwe@gmail.com>
2022-12-23 13:05:39 -0800

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ *
+ * @c - int value to be compared
+ *
+ * Return: 1 if c is a letter, else 0.
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
