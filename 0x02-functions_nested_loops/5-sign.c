e7b46d5f97557282b9523475cca6a0b3301d4709
Isaac Adigwe <isaacadigwe@gmail.com>
2022-12-23 13:36:24 -0800

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * sign - prints the sign of a number
+ *
+ * @n: int value to be compared
+ *
+ * Return: 1 if n is greater than zero
+ * 0 if n is zero
+ * -1 if n is less than zero
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return(1);
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return(0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+	_putchar('\n');
+	return (0);
+}
