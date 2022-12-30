da7bfa8d9090cc095c86ef766b1c729de0d12d93
Isaac Adigwe <isaacadigwe@gmail.com>
2022-12-30 11:17:54 -0800

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ *
+ * @i: first integer
+ * @j: second integer
+ *
+ * Return: result of i * j.
+ */
+
+int add(int i, int j)
+{
+	return (i + j);
+}
