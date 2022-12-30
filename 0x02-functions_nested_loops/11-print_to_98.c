037e391472d89d95ea2ca55269b17ea429f0c036
Isaac Adigwe <isaacadigwe@gmail.com>
2022-12-30 13:40:38 -0800

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include "6-abs.c"
+#include <stdio.h>
+/**
+ * print_to_98 - prints natural numbers from n to 98
+ *
+ * @n: integer to be compared
+ *
+ * Return: Always 0.
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	printf("\n");
+}
